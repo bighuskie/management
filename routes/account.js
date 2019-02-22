50706d19af0762795e10973f1963e2cdb4ffc8a4
B.Huskie <1665370368@qq.com>
2019-02-22 17:00:22 +0800

    完成个人中心信息页渲染，更改用户信息背景显示等样式

--- routes/account.js
+++ routes/account.js
@@ -4,13 +4,12 @@ const userDB = require("../models/users");
 router.prefix("/account");
 
 router.get("/", async (ctx, next) => {
-  console.log(ctx.session.user)
-  let userInfo = await userDB.findById(ctx.session.user.id);
-  // console.log(userInfo)
+  let userId = ctx.session.user._id;
+  let userInfo = await userDB.findById(userId);
   await ctx.render("./account/profile", {
     title: "账号信息",
-    user: ctx.session.user
-    // userInfo
+    user: ctx.session.user,
+    userInfo
   });
 });
 
