edbbd204b3a26676790b769cb51de9551c452a15
B.Huskie <1665370368@qq.com>
2019-02-22 20:44:12 +0800

    修改个人中心信息展示样式，完成工人设置布局及保存数据到数据库

--- public/javascripts/client/main.js
+++ public/javascripts/client/main.js
@@ -1,8 +1,50 @@
+/**
+ * 说明：
+ *  由于在ejs模板中统一引用jquery，所以统一将客户端的逻辑处理写在此文件，注意区分
+ */
+
 $(document).ready(function() {
   /**
    * 显示和隐藏api接口列表项
    */
   $(".api-control").on("click", function() {
     $(".api-wrapper").toggle("slow");
+  });
+
+  /**
+   * 用户个人中心设置列表样式控制
+   */
+  $(".account-wrapper li:not('.title')").on("click", function() {
+    $(this)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+  });
+
+  /**
+   * 用户设置页发送请求逻辑
+   */
+  $("#userSettings").on("submit", function(e) {
+    e.preventDefault();
+    let user = $("input[name='username']").val();
+    let email = $("input[name='email']").val();
+    if (user && email) {
+      let formdata = $(this).serialize();
+      $.ajax({
+        type: "post",
+        url: "/account/settings",
+        data: formdata,
+        dataType: "json",
+        success: function(res) {
+          alert("更改成功");
+        },
+        error: function(err) {
+          alert("更改失败");
+          console.log(err);
+        }
+      });
+    } else {
+      alert("输入框不可以为空");
+    }
   });
 });
--- routes/account.js
+++ routes/account.js
@@ -13,8 +13,25 @@ router.get("/", async (ctx, next) => {
   });
 });
 
-router.get("/setting", async (ctx, next) => {
-  ctx.body = "this is a users/bar response";
+/**
+ * 账号设置逻辑处理
+ */
+router.get("/settings", async (ctx, next) => {
+  let userId = ctx.session.user._id;
+  let userInfo = await userDB.findById(userId);
+  await ctx.render("./account/settings", {
+    title: "账号设置",
+    user: ctx.session.user,
+    userInfo
+  });
+});
+
+router.post("/settings", async (ctx, next) => {
+  let formReq = ctx.request.body;
+  let userId = ctx.session.user._id;
+  await userDB.findByIdAndUpdate(userId, formReq).then(user => {
+    ctx.status = 204;
+  });
 });
 
 module.exports = router;
